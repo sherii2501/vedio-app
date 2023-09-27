b74add147ceb3ee4dc1e185bab9a1ea3a218db54
sherii2501 <sherinsherii24@gmail.com>
2023-09-27 15:24:16 +0530

    vedio player app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Media Player App</h1>
+      <LandingPage />
+      <Home />
     </div>
   );
 }
